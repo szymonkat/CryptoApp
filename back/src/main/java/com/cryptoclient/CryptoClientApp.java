e6dab0599000216e150302bf6f9d9bf389eb78fc
szymonkat <szymonkat@gmail.com>
2021-03-18 20:41:45 +0100

    Postgres conversion

--- back/src/main/java/com/cryptoclient/CryptoClientApp.java
+++ back/src/main/java/com/cryptoclient/CryptoClientApp.java
@@ -2,6 +2,7 @@ package com.cryptoclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -9,6 +10,11 @@ public class CryptoClientApp extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CryptoClientApp.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure (SpringApplicationBuilder springApplicationBuilder) {
+		return springApplicationBuilder.sources(CryptoClientApp.class);
 	}
 
 }
